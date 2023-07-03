f499da4c04918b74bd6bcf60ee663ca882b26887
Aditya <121721549+adityaSalame@users.noreply.github.com>
2023-07-03 11:47:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+
+let d=document.createElement("div");
+document.body.appendChild(d);
+
+d.innerHTML="You are using " + navigator.appName + " version " + navigator.appVersion;
+
+// document.getElemntsByTagName("div")[0].id="browser-info";
+// document.write(document.getElemntsByTagName("div")[0].id);
+
+// let text=document.createTextNode("You are using " + navigator.appName + " version " + navigator.appVersion);
+// d.appendChild(text);
